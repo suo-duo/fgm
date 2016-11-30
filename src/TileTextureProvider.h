08b6534197ad23fdb3cb9a7385134087cde94726
Michael H. Suo <suo@fb.com>
2016-11-30 00:30:11 -0500

    Actually add TileTextureProvider

---
+++ src/TileTextureProvider.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <unordered_map>
+#include <SFML/Graphics.hpp>
+
+
+enum class TileType { LAVA, DIRT, GRASS, STONE };
+
+class TileTextureProvider {
+public:
+  TileTextureProvider();
+
+  TileTextureProvider(const TileTextureProvider&) = delete;
+  TileTextureProvider& operator=(const TileTextureProvider&) = delete;
+  TileTextureProvider(TileTextureProvider&&) = default;
+  TileTextureProvider& operator=(TileTextureProvider&&) = default;
+
+  sf::Texture& getTileTexture(const TileType tileType);
+
+private:
+  std::unordered_map<TileType, sf::Texture> tileSpriteMap_;
+};
